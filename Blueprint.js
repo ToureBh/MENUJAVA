5ed86ae8c7e6c5165fcd790e1d3fbfe8e4d48dea
ToureBh <149252642+ToureBh@users.noreply.github.com>
2024-07-17 16:22:33 +0200

    Add files via upload

--- Blueprint.js
+++ Blueprint.js
@@ -1,168 +1,157 @@
-class menuDinerBottega {
-    constructor () {
-        this.menus = {
-            breakfast: {
-                starters: [
-                    { name: "Fruit mix", price: 4},
-                    { name: "Chocolate", price: 4},
-                    { name: "Gelato mix", price: 4}
-                ],
-                mains: [
-                    { name: "Bacon & Eggs", price: 4},
-                    { name: "Sweets", price: 4},
-                    { name: "Biscuit", price: 4}
-                ],
-                drinks: [
-                    { name: "Coffee", price: 4},
-                    { name: "Milk", price: 4},
-                    { name: "Tea", price: 4}
-                ]
-            },
-            lunch: {
-                starters: [
-                    { name: "Ham and Cheese", price: 4},
-                    { name: "Bread ", price: 4},
-                    { name: "Butter", price: 4}
-                ],
-                mains: [
-                    { name: "Bistec", price: 4},
-                    { name: "Mash potatoes", price: 4},
-                    { name: "Lasagna", price: 4}
-                ],
-                drinks: [
-                    { name: "Aperol", price: 4},
-                    { name: "Coffe", price: 4},
-                    { name: "Beer", price: 4}
-                ]
-            },
-            dinner:{
-                starters: [
-                    { name: "Dry fruits", price: 4},
-                    { name: "Bread", price: 4},
-                    { name: "Coconut", price: 4}
-                ],
-                mains: [
-                    { name: "Pizza", price: 4},
-                    { name: "Pasta", price: 4},
-                    { name: "Lasagna", price: 4}
-                ],
-                drinks: [
-                    { name: "Aperol", price: 4},
-                    { name: "Beer", price: 4},
-                    { name: "Cocktails", price: 4}
-                ]
-            },
-        
-        };
-        this.comments = [
-            "Well well... somebody knows a thing or two!!",
-            "I would do the same Sir!",
-            "Please!! it is a good choice",
-            "I think you choose the best!",
-            "Let me ask in the kitchen butI think it is fine"
-        ];
+const menus = {
+    breakfast: {
+        starters: [
+            { name: "Fruit mix", price: 4.99},
+            { name: "Chocolate", price: 2.30},
+            { name: "Gelato mix", price: 1.50}
+        ],
+        mains: [
+            { name: "Bacon & Eggs", price: 6.60},
+            { name: "Sweets", price: 6.50},
+            { name: "Biscuit", price: 1.50}
+        ],
+        drinks: [
+            { name: "Coffee", price: 1.50},
+            { name: "Milk", price: 1.50},
+            { name: "Tea", price: 2.50}
+        ]
+    },
+    lunch: {
+        starters: [
+            { name: "Ham and Cheese", price: 7.60},
+            { name: "Bread ", price: 3.00},
+            { name: "Butter", price: 1.40}
+        ],
+        mains: [
+            { name: "Bistec", price: 19.50},
+            { name: "Mash potatoes", price: 12.10},
+            { name: "Lasagna", price: 8.50}
+        ],
+        drinks: [
+            { name: "Aperol", price: 6.50},
+            { name: "Coffe", price: 1.50},
+            { name: "Beer", price: 4.00}
+        ]
+    },
+    dinner:{
+        starters: [
+            { name: "Dry fruits", price: 5.50},
+            { name: "Bread", price: 1.50},
+            { name: "Coconut", price: 4.00}
+        ],
+        mains: [
+            { name: "Pizza", price: 10.20},
+            { name: "Pasta", price: 9.60},
+            { name: "Lasagna", price: 8.50}
+        ],
+        drinks: [
+            { name: "Aperol", price: 6.50},
+            { name: "Beer", price: 4.00},
+            { name: "Cocktails", price: 7.60}
+        ]
     }
+};
+       
+
+// Comentarios de la camarera
+const comments = [
+    "Well well... somebody knows a thing or two!!",
+    "I would do the same Sir!",
+    "Please!! it is a good choice",
+    "I think you choose the best!",
+    "Let me ask in the kitchen butI think it is fine"
+];
 
 
-    // Función para determinar el momento de la comida basado en la hora ingresada
-    getMenuType(hora) {
-        if (hora >= 8 && hora < 13) return "breakfast";
-        else if (hora >= 13 && hora < 18) return "lunch";
-        else if (hora >= 18 && hora < 23) return "dinner";
-        else return "closed"
-    }
-
-    // Función para seleccionar un comentario aleatorio
-    obtenerComentarioAleatorio(comments) {
+// Función para seleccionar un comentario aleatorio
+function obtenerComentarioAleatorio(comments) {
     const indice = Math.floor(Math.random() * comments.length);
     return comments[indice];
-    }
+}
 
-    getOrder(hora) {
-        const tipoMenu = this.getMenuType(hora);
-        if (tipoMenu === 'closed') {
-            alert("Sorry, the restaurant is closed.");
-            return;
-        } else {
-            this.printMenu(tipoMenu);
+
+// Función para mostrar el menú completo y permitir la selección
+function seleccionarArticulo(menu, tipo, tiempo) {
+    let mensaje = `${tiempo.charAt(0).toUpperCase() + tiempo.slice(1)} Menu - ${tipo.charAt(0).toUpperCase() + tipo.slice(1)}:\n\n`;
+    menu.forEach((articulo, index) => {
+        mensaje += `${index + 1}. ${articulo.name} - €${articulo.price.toFixed(2)}\n`;
+    });
+
+    let seleccionNumerica = NaN;
+    while (isNaN(seleccionNumerica) || seleccionNumerica < 1 || seleccionNumerica > menu.length) {
+        let seleccion = prompt(`\n${mensaje}\nSelect a ${tipo} (1-${menu.length}):`);
+        if (seleccion === null) {
+            alert("Proceso cancelado.");
+            return null;
+        }
+        seleccionNumerica = parseInt(seleccion, 10);
+        if (isNaN(seleccionNumerica) || seleccionNumerica < 1 || seleccionNumerica > menu.length) {
+            alert("Invalid input. Please, enter a vaalid number inside the menu.");
         }
     }
 
-    // Función para mostrar el menú completo y permitir la selección
-    printMenu(tipoMenu) {
-        const menu = this.menus[tipoMenu];
-        let menuString = `${tipoMenu.charAt(0).toUpperCase() + tipoMenu.slice(1)} Menu. \n\Starters:\n`;
-        menu.starters.forEach(item => {
-            menuString += `${item.name}: ${item.price}€\n`;
-        });
-        menuString += `\nMains:\n`;
-        menu.mains.forEach(item => {
-            menuString += `${item.name}: ${item.price}€\n`;
-        });
-        menuString += `\nDesserts:\n`;
-        menu.mains.forEach(item => {
-            menuString += `${item.name}: ${item.price}€\n`;
-        });
+    const articulo = menu[seleccionNumerica - 1];
+    alert(obtenerComentarioAleatorio(comments));
+    return articulo;
+}
 
-        alert(menuString);
+
+// Función para determinar el momento de la comida basado en la hora ingresada
+function obtenerMomentoComida(hora) {
+    const [hours, minutes] = hora.split(':').map(Number);
+    
+    if (hours < 8 || hours >= 24 || (hours === 23 && minutes > 0) || isNaN(hours) || isNaN(minutes)) {
+        return null;
     }
-
-    selections(menu, category){
-        let item = null;
-        while (!item) {
-            let selectionPrompt = `Please select a ${category.slice(0, -1)}`
-            menu[category].forEach( item => {
-                selectionPrompt += `${item.name} (${item.price})`
-            });
-            const selection = prompt(selectionPrompt);
-            item = menu[category].find(item => item.name.toLowerCase())
-            if (item) {
-                const comment = this.comments[Math.random()];
-                alert(`You selected ${item.name}. ${comment}. It costs ${item.price}`)
-            } else {
-                alert("Item not found. Please choose a valid item")
-            }
-        }
-        return item;
+    
+    if (hours >= 8 && hours <= 10) {
+        return 'breakfast';
+    } else if (hours >= 11 && hours <= 16) {
+        return 'lunch';
+    } else if (hours >= 17 && hours <= 23) {
+        return 'dinner';
+    } else {
+        return null;
     }
+}
 
-    // Función principal para ejecutar el menú
-    ejecutarMenu() {
+
+// Función principal para ejecutar el menú
+function ejecutarMenu() {
     let momentoComida = null;
     while (!momentoComida) {
-        const horaSeleccionada = prompt("Bienvenido a Bottega fast Food, horario atencion 8am a 23pm, Ingresa la hora (HH) para seleccionar tu comida:");
+        const horaSeleccionada = prompt("Welcome to the Bottega fast Food Restaurant, opening hours are from 08:00am to 23:00pm, Please enter current time (HH:MM) to select the menu:");
         if (horaSeleccionada === null) {
-            alert("Proceso cancelado.");
+            alert("Process cancelled.");
             return;
         }
-        momentoComida = this.getMenuType(horaSeleccionada);
+        momentoComida = obtenerMomentoComida(horaSeleccionada);
         if (!momentoComida) {
-            alert("Hora no válida. Por favor, ingresa una hora dentro de las franjas horarias permitidas.");
-            }
+            alert("this time input is not valid. Please, enter a valid time with the correct format (HH:MM)");
         }
-
-        // Selección de plato principal
-        const platoStarters = this.printMenu(this.menus[tipoMenu].starters, 'starter', momentoComida);
-        if (!platoStarters) return;  // Si el usuario cancela
-
-
-        // Selección de guarniciones
-        const platoMain = this.printMenu(this.menus[tipoMenu].mains, 'plato principal', momentoComida);
-        if (!platoMain) return;  // Si el usuario cancela
-
-
-        const opcionDrinks = this.printMenu(this.menus[tipoMenu].drinks, 'bebida', momentoComida);
-        if (!opcionDrinks) return;  // Si el usuario cancela
-
-
-        // Sumar el costo total
-        const costoTotal = platoStarters.price + platoMain.price + opcionDrinks.price;
-        alert(`Bottega Fast Food\n\nRecibo venta\n${platoStarters.name} = €${platoStarters.price}\n${platoMain.name} = €${platoMain.price}\n${opcionDrinks.name} = €${opcionDrinks.price} \n\nCosto total: €${costoTotal.toFixed(2)}\nGracias por Visitarnos`);
     }
 
-};
+    const menu = menus[momentoComida];
+
+    // Selección de plato principal
+    const platoStarters = seleccionarArticulo(menu.starters, 'starters', momentoComida);
+    if (!platoStarters) return;  // Si el usuario cancela
 
 
-skj = new menuDinerBottega;
+    // Selección de guarniciones
+    const platoMain = seleccionarArticulo(menu.mains, 'mains', momentoComida);
+    if (!platoMain) return;  // Si el usuario cancela
 
-skj.ejecutarMenu();
+
+    const platoDrinks = seleccionarArticulo(menu.drinks, 'drinks', momentoComida);
+    if (!platoDrinks) return;  // Si el usuario cancela
+
+
+    // Sumar el costo total
+   const costoTotal = platoStarters.price + platoMain.price + platoDrinks.price;
+    alert(`Bottega Fast Food\n\nCheck\n${platoStarters.name} = €${platoStarters.price}\n${platoMain.name} = €${platoMain.price}\n${platoDrinks.name} = €${platoDrinks.price} \n\nTotal price: €${costoTotal.toFixed(2)}\nThanks for visiting!!`);
+}
+
+
+// Ejecutar el menú
+ejecutarMenu();
